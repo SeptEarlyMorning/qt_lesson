a8de7f7ba3ce03b9b8df0659f5c18d5bbf43f6d0
SeptEarlyMorning <1790732099@qq.com>
2019-10-12 14:36:09 +0800

    json 数据传输

---
+++ js/json_data/app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+    // res.end('asdfnode');
+    res.setHeader('Content-Type', 'text/html;charset=utf-8');
+    if (req.url == '/') {
+        // res.end('首页a');
+        let stream = fs.createReadStream(__dirname + '/index.html');
+        res.setHeader('Content-Type', 'text/html;charset=utf-8');
+        stream.pipe(res);
+    } else if (req.url == '/book') {
+        res.setHeader('Content-Type', 'text/json;charset=utf-8');
+        const data = {
+            "name": "Node.js 入门示例",
+            "description": "学好node, 做全栈开发",
+            "date": "2019-8-23"
+        }
+        const result = JSON.stringify(data);
+        res.end(result);
+    }
+}).listen(6515);
