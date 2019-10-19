1b3a6e05699868be00bece98569a118d30e2708c
SeptEarlyMorning <1790732099@qq.com>
2019-10-19 20:33:03 +0800

    vue 图片放大器

---
+++ vue/good-hover-zoom-image/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <hover-zoom
+      bigSrc="https://img.alicdn.com/imgextra/i3/1714128138/O1CN01K14VAw29zFjiFDawl_!!1714128138.jpg"
+      smallSrc="https://img.alicdn.com/imgextra/i3/1714128138/O1CN01K14VAw29zFjiFDawl_!!1714128138.jpg_430x430q90.jpg"
+    ></hover-zoom>
+  </div>
+</template>
+
+<script>
+import HoverZoom from "./components/HoverZoom";
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    HoverZoom
+  }
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
