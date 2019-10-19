98000b3b7124a80bd5341f54d99a779eee5415eb
SeptEarlyMorning <1790732099@qq.com>
2019-10-19 20:24:42 +0800

    vue 获取当前秒数，实时更新

---
+++ vue/clock/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <Second :second="second" />
+    <Minute :Minute="Minute" />
+    <Hour :Hour="Hour" />
+    <Apm :Apm="Apm" />
+    <Week :Week="Week" />
+    <Day :Day="Day" />
+    <Month :Month="Month" />
+  </div>
+</template>
+
+<script>
+import Second from "@/components/Second.vue";
+import Minute from "@/components/Minute.vue";
+import Hour from "@/components/Hour.vue";
+import Apm from "@/components/Apm.vue";
+import Week from "@/components/Week.vue";
+import Day from "@/components/Day.vue";
+import Month from "@/components/Month.vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      second: ""
+    };
+  },
+  components: {
+    Second,
+    Minute,
+    Hour,
+    Apm,
+    Week,
+    Day,
+    Month
+  },
+  methods: {
+    start() {
+      setInterval(() => {
+        let date = new Date();
+        this.second = date.getSeconds();
+      }, 1000);
+    }
+  },
+  created() {
+    this.start();
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
