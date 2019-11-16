7e2939120c2828cf1fe3ebe6660f8159d4b0ef01
SeptEarlyMorning <1790732099@qq.com>
2019-11-16 19:39:58 +0800

    生产周期 react

---
+++ react/react-16/react-lifecycle/src/Son.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import SubSon from './SubSon';
+
+class Son extends Component {
+    render() {
+        return (
+            <SubSon />
+        )
+    }
+}
+
+export default Son;
