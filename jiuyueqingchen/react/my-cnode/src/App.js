0d1b0aead391f71f7684c720c595a577473907a1
SeptEarlyMorning <1790732099@qq.com>
2020-04-11 23:55:25 +0800

    cnode 项目的基本框架

---
+++ jiuyueqingchen/react/my-cnode/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './static/css/App.css';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>button</Button>
+    </div>
+  );
+}
+
+export default App;
